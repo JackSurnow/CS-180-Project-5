c044f1cf535b2614c83612ef0a83617179e4eed3
JackSurnow <89482833+JackSurnow@users.noreply.github.com>
2021-12-05 02:37:45 -0500

    Add files via upload

---
+++ src/PostReader.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * Purdue University -- CS18000
+ * Project 4
+ *
+ * @author Jason Bodzy, Jack Surnow, Jack Weston, Irfan Hussain, and Jungeun Hwang
+ * @version 1.0
+ */
+
+public class PostReader { //Reads the master post class and individual post files
+    public ArrayList<String> readPost(int postNumber) { // Reads the individual post
+        String fileName = "Post" + postNumber + ".txt";
+        ArrayList<String> post = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                post.add(line);
+                line = bufferedReader.readLine();
+            }
+
+            return post;
+        } catch (Exception e) {
+            var failed = new ArrayList<String>();
+            failed.add("There was an error reading this post, ensure the post file is correct!");
+            e.printStackTrace();
+            return failed;
+        }
+    }
+}
