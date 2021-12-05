13b98cbb0deec302dbacdd4bf850f5f4d0a914c2
JackSurnow <89482833+JackSurnow@users.noreply.github.com>
2021-12-05 18:44:49 -0500

    Delete src directory

--- src/Time.java
+++
@@ -1,8 +0,0 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class Time {
-    public static String time() {
-        return new SimpleDateFormat("HH:mm").format(new Date());
-    }
-}
